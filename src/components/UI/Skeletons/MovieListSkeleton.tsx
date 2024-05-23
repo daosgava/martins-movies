8682baf7633325839e8d9064307968b853c827d3
daosgava <daosgava.garcia@gmail.com>
2024-05-24 01:19:22 +1000

    adjust indentation

--- src/components/UI/Skeletons/MovieListSkeleton.tsx
+++ src/components/UI/Skeletons/MovieListSkeleton.tsx
@@ -2,9 +2,11 @@ const ProductListSkeleton = () => {
 	return (
 		<div className="my-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
 			{Array.from({ length: 8 }).map((_, index) => (
-				<div key={index} className="relative bg-slate-300 rounded-xl overflow-hidden animate-pulse">
-					<div className="h-[500px] w-[300px]">
-					</div>
+				<div
+					key={index}
+					className="relative bg-slate-300 rounded-xl overflow-hidden animate-pulse"
+				>
+					<div className="h-[500px] w-[300px]"></div>
 				</div>
 			))}
 		</div>
