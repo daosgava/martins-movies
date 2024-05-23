1d53e4e9300711138d5d6ad46f6642360e05df69
daosgava <daosgava.garcia@gmail.com>
2024-05-23 22:56:58 +1000

    add favorite and share icons

--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -1,6 +1,8 @@
 import { FC } from "react";
 import type { Movie } from "@/types";
 import Button from "@/components/UI/Button/Button";
+import Favorite from "../UI/Icons/Favorite";
+import Share from "../UI/Icons/Share";
 
 type MovieProps = {
 	movie: Movie;
@@ -9,6 +11,11 @@ type MovieProps = {
 const Movie: FC<MovieProps> = ({ movie }) => {
 	return (
 		<div className="group relative bg-blue-900 rounded-xl overflow-hidden">
+			<div className="absolute top-0 left-0 p-2 rounded-bl-xl z-10 opacity-80 group-hover:opacity-100 flex flex-row justify-between w-full">
+				<Favorite />
+				<Share />
+			</div>
+
 			<img
 				src={
 					movie.poster_path
@@ -18,7 +25,6 @@ const Movie: FC<MovieProps> = ({ movie }) => {
 				alt={movie.title}
 				className="relative h-full w-full object-cover object-center lg:h-full lg:w-full opacity-80 group-hover:opacity-100"
 			/>
-
 			<div className="absolute bottom-0 mt-4 flex flex-col justify-between p-2 w-full h-[40%] bg-gradient-to-t from-slate-900 from-90% md:from-70% group-hover:from-20%  to-transparent">
 				<h3 className="text-lg text-white text-center mt-4">
 					<a href="#">{movie.title}</a>
