9e1287388a48056073c315fc153ab216998620be
daosgava <daosgava.garcia@gmail.com>
2024-05-23 23:40:40 +1000

    add rating

--- src/components/Movie/Movie.tsx
+++ src/components/Movie/Movie.tsx
@@ -3,6 +3,8 @@ import type { Movie } from "@/types";
 import Button from "@/components/UI/Button/Button";
 import Favorite from "../UI/Icons/Favorite";
 import Share from "../UI/Icons/Share";
+import Star from "../UI/Icons/Star";
+import EmptyStar from "../UI/Icons/EmptyStar";
 
 type MovieProps = {
 	movie: Movie;
@@ -20,12 +22,23 @@ const Movie: FC<MovieProps> = ({ movie }) => {
 				src={
 					movie.poster_path
 						? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-						: "https://dummyimage.com/300x500/eeeeee/767676&text=IMAGE+NOT+AVAILABLE"
+						: "https://dummyimage.com/300x500/eeeeee/767676&text=No+Image+Available"
 				}
 				alt={movie.title}
 				className="relative h-full w-full object-cover object-center lg:h-full lg:w-full opacity-80 group-hover:opacity-100"
 			/>
-			<div className="absolute bottom-0 mt-4 flex flex-col justify-between p-2 w-full h-[40%] bg-gradient-to-t from-slate-900 from-90% md:from-70% group-hover:from-20%  to-transparent">
+			<div className="absolute bottom-0 flex flex-col justify-between p-2 w-full h-[46%] bg-gradient-to-t from-slate-900 md:from-70% group-hover:from-20%  to-transparent">
+				<div className="flex justify-center">
+					{Array.from({ length: 5 }).map((_, i) => (
+						<span key={i}>
+							{Number(movie.vote_average) / 2 > i ? (
+								<Star />
+							) : (
+								<EmptyStar />
+							)}
+						</span>
+					))}
+				</div>
 				<h3 className="text-lg text-white text-center mt-4">
 					<a href="#">{movie.title}</a>
 				</h3>
--- src/components/UI/Icons/Share.tsx
+++ src/components/UI/Icons/Share.tsx
@@ -11,8 +11,8 @@ const Share = () => (
 		>
 			<path
 				stroke="currentColor"
-				stroke-linecap="round"
-				stroke-width="1.5"
+				strokeLinecap="round"
+				strokeWidth="1.5"
 				d="M7.926 10.898 15 7.727m-7.074 5.39L15 16.29M8 12a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Zm12 5.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Zm0-11a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"
 			/>
 		</svg>
