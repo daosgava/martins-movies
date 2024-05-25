a298b89a3520fa99771ec8dff5e8c3dd357e46b0
daosgava <daosgava.garcia@gmail.com>
2024-05-25 21:15:13 +1000

    change header color

--- src/components/Sections/Header/Header.tsx
+++ src/components/Sections/Header/Header.tsx
@@ -1,5 +1,5 @@
 const Header = () => (
-	<div className="sticky md:relative z-20 flex bg-gradient-to-r from-sky-500 via-cyan-600 to-blue-950 px-6 py-6 md:py-32 lg:px-8">
+	<div className="sticky md:relative z-20 flex bg-gradient-to-r from-blue-500 to-sky-500 px-6 py-6 md:py-32 lg:px-8">
 		<div className="mx-auto max-w-2xl text-center">
 			<h2 className="text-4xl font-bold tracking-tight text-white md:text-6xl">
 				Martin&apos;s Movie Finder
